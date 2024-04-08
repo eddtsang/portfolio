f9a049cd66ce7d3f2731e5a0d7ab8193e5790ef1
Eddie <edd.tsang@gmail.com>
2024-04-08 18:25:38 -0500

    Base line modification, initial checkpoint

---
+++ ghi/src/nav/nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+        <nav>
+            <div>About Me</div>
+            <div>Projects</div>
+            <div>Contact</div>
+        </nav>
+    )
+}
+
+export default Nav;
