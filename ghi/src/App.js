814e4fc9a54729501ffe83d370217b9604a4434c
Eddie <edd.tsang@gmail.com>
2025-07-30 16:40:19 -0500

    delete nav component and change some code for app

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,32 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Nav from './nav/nav';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Nav from './components/Nav/Nav'
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Nav />
-      </header>
-      <body>
-        <img src={logo} className="App-logo" alt="logo" />
-          <p>Test</p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-      </body>
-      <footer>
-        <h3>Test Footer</h3>
-      </footer>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
