b7c51be21ba2a8be5affed47b832ab2ed17ad842
Eddie <edd.tsang@gmail.com>
2025-08-07 01:12:09 -0500

    modified content on Nav

--- ghi/src/components/Nav/Nav.jsx
+++ ghi/src/components/Nav/Nav.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
 import styles from "./Nav.module.css";
 
 function Nav() {
     return (
         <nav className={styles.nav}>
-            <div>About Me</div>
-            <div>Projects</div>
-            <div>Contact</div>
+            <Link to="/"><div>Home</div></Link>
+            <Link to="/aboutme"><div>About Me</div></Link>
+            <Link to="/projects"><div>Projects</div></Link>
+            <Link to="/contact"><div>Contact</div></Link>
         </nav>
     );
 };
