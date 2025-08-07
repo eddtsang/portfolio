4370a247db5eb2e6958c71c2c0005f10ce812282
Eddie <edd.tsang@gmail.com>
2025-08-07 01:10:38 -0500

    made changes to footer content and styling

--- ghi/src/components/Footer/Footer.jsx
+++ ghi/src/components/Footer/Footer.jsx
@@ -1,10 +1,19 @@
  import styles from "./Footer.module.css";
 
+ import github_dark from '../../assets/logos/github-mark.png';
+ import gitlab_dark from 'assets/logos/gitlab_logo_black.png';
+ import linked_dark from 'assets/logos/InBug-Black.png';
+
 function Footer() {
     console.log("check")
     return(
-        <footer className={styles.Footer}>
-            <h3>Test Footer 1</h3>
+        <footer className={styles.footer}>
+            <p>© 2025 Eddie Tsang</p>
+            <div>
+                <img src={gitlab_dark} alt="GitLab" />
+                <img src={github_dark} alt="GitHub" />
+                <img src={linked_dark} alt="LinkedIn" />
+            </div>
         </footer>
     );
 };
