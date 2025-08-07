d2d50e16148fc95c85b9d02727ad67c43fef7dc6
Eddie <edd.tsang@gmail.com>
2025-08-07 01:13:45 -0500

    added routes at App.js

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,18 +1,33 @@
 
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
-import Main from './components/Main/Main';
 import Nav from './components/Nav/Nav'
 import Footer from './components/Footer/Footer';
 
+import Home from './pages/Home/Home';
+import AboutMe from './pages/AboutMe/AboutMe';
+import Projects from './pages/Projects/Projects';
+import ProjectDetails from './pages/ProjectDetails/ProjectDetails';
+import Contact from './pages/Contact/Contact';
+
+
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <Nav />
-      <Main />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutme" element={<AboutMe />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:id" element={<ProjectDetails />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
