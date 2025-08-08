fbce6c7e2820adbb3c2470df1a06e75e4d9c9b57
Eddie <edd.tsang@gmail.com>
2025-08-08 15:09:51 -0500

    best practice for adding ; to a few lines

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
-import Nav from './components/Nav/Nav'
+import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 
 import Home from './pages/Home/Home';
