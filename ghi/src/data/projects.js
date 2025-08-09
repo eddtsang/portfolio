103a19593fb560f5d9d648746f9c60bd2f62374e
Eddie <edd.tsang@gmail.com>
2025-08-09 01:11:46 -0500

    created initial file for project data so it can be pulled into other pages or components

---
+++ ghi/src/data/projects.js
@@ -0,0 +1,12 @@
+const projects = [
+    {
+        id: 1,
+        title: "test1 title",
+        description: "test1 descript",
+        image: "test1 image",
+        techStack: ["test1", "test1"],
+        repo: "test1"
+    }
+]
+
+export default projects;
