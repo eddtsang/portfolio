61ac68c7300948b4b9b35fb268360eb95f92bc76
Eddie <edd.tsang@gmail.com>
2025-08-09 01:15:21 -0500

    modified projects page to pull in data and show info from data

--- ghi/src/pages/Projects/Projects.jsx
+++ ghi/src/pages/Projects/Projects.jsx
@@ -1,10 +1,19 @@
 import styles from "./Projects.module.css";
+import proj_data from "../../data/projects";
 
 function  Projects() {
     return (
         <>
             <h1>Projects</h1>
             <div className={styles.project_grid}>
+                {proj_data.map( project => (
+                    <div>
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                    </div>
+
+                    )
+                )}
             </div>
         </>
     );
