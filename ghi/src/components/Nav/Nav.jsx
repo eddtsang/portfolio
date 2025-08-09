40eeb470883f994496045f6f7a7643e66f6d6078
Eddie <edd.tsang@gmail.com>
2025-08-09 01:14:08 -0500

    changed nav to buttons instead of links and modified css for them

--- ghi/src/components/Nav/Nav.jsx
+++ ghi/src/components/Nav/Nav.jsx
@@ -1,13 +1,27 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import styles from "./Nav.module.css";
 
 function Nav() {
     return (
         <nav className={styles.nav}>
+            <a href="/">
+                <button className={styles.button}>Home</button>
+            </a>
+            <a href="/aboutme">
+                <button className={styles.button}>About Me</button>
+            </a>
+            <a href="/projects">
+                <button className={styles.button}>Projects</button>
+            </a>
+            <a href="contact">
+                <button className={styles.button}>Contact</button>
+            </a>
+            {/*
             <Link to="/"><div>Home</div></Link>
             <Link to="/aboutme"><div>About Me</div></Link>
             <Link to="/projects"><div>Projects</div></Link>
             <Link to="/contact"><div>Contact</div></Link>
+            */}
         </nav>
     );
 };
