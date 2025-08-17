3caebccc79cede4b4b005b6794802e5f41e40086
Eddie <edd.tsang@gmail.com>
2025-08-17 00:20:37 -0500

    modified project page and its css

--- ghi/src/pages/Projects/Projects.jsx
+++ ghi/src/pages/Projects/Projects.jsx
@@ -1,19 +1,15 @@
 import styles from "./Projects.module.css";
 import proj_data from "../../data/projects";
+import ProjectTile from "../../components/ProjectTile/ProjectTile";
 
-function  Projects() {
+function Projects() {
     return (
         <>
             <h1>Projects</h1>
             <div className={styles.project_grid}>
-                {proj_data.map( project => (
-                    <div>
-                        <h3>{project.title}</h3>
-                        <p>{project.description}</p>
-                    </div>
-
-                    )
-                )}
+                {proj_data.map(project => (
+                    <ProjectTile key={project.id} project={project}/>
+                ))}
             </div>
         </>
     );
